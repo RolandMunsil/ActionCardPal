2f17280510bb780078cff33371ae913265b170c5
Roland Munsil <roland@munsil.com>
2025-06-24 22:00:50 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿using ImGuiNET;
+using RetroRoulette;
+using System.Diagnostics;
+using System.Text.Json;
+using System.Xml.Linq;
+using Veldrid.Sdl2;
+using Veldrid.StartupUtilities;
+using Veldrid;
+
+namespace ActionCardPal
+{
+    internal class Program
+    {
+        static ImFontPtr font20;
+        static ImFontPtr font30;
+        static ImFontPtr font40;
+
+        static bool renderImguiDemo = false;
+
+        static void Main(string[] args)
+        {
+            Sdl2Window window;
+            GraphicsDevice gd;
+
+            // Create window, GraphicsDevice, and all resources necessary
+
+            VeldridStartup.CreateWindowAndGraphicsDevice(
+                new WindowCreateInfo(50, 50, 1280, 720, WindowState.Normal, "ActionCardPal"),
+                new GraphicsDeviceOptions(true, null, true, ResourceBindingModel.Improved, true, true),
+                out window,
+                out gd);
+
+            ImGui.CreateContext();
+            ImGuiIOPtr io = ImGui.GetIO();
+            io.BackendFlags |= ImGuiBackendFlags.RendererHasVtxOffset;
+            io.ConfigFlags |= ImGuiConfigFlags.NavEnableKeyboard | ImGuiConfigFlags.DockingEnable;
+            io.Fonts.Flags |= ImFontAtlasFlags.NoBakedLines;
+
+            font20 = io.Fonts.AddFontFromFileTTF(@"C:\Windows\Fonts\segoeui.ttf", 20);
+            font30 = io.Fonts.AddFontFromFileTTF(@"C:\Windows\Fonts\segoeui.ttf", 30);
+            font40 = io.Fonts.AddFontFromFileTTF(@"C:\Windows\Fonts\segoeui.ttf", 40);
+
+            CommandList cl = gd.ResourceFactory.CreateCommandList();
+            ImGuiController controller = new ImGuiController(gd, gd.MainSwapchain.Framebuffer.OutputDescription, window.Width, window.Height);
+
+            window.Resized += () =>
+            {
+                gd.MainSwapchain.Resize((uint)window.Width, (uint)window.Height);
+                controller.WindowResized(window.Width, window.Height);
+            };
+
+            Stopwatch dtStopwatch = Stopwatch.StartNew();
+
+            // Main application loop
+            while (window.Exists)
+            {
+                float deltaTime = (float)dtStopwatch.Elapsed.TotalSeconds;
+                dtStopwatch.Restart();
+                InputSnapshot snapshot = window.PumpEvents();
+                if (!window.Exists) { break; }
+                controller.Update(deltaTime, snapshot); // Feed the input events to our ImGui controller, which passes them through to ImGui.
+
+                RenderUI();
+
+                cl.Begin();
+                cl.SetFramebuffer(gd.MainSwapchain.Framebuffer);
+                cl.ClearColorTarget(0, new RgbaFloat(1, 1, 1, 1));
+                controller.Render(gd, cl);
+                cl.End();
+                gd.SubmitCommands(cl);
+                gd.SwapBuffers(gd.MainSwapchain);
+            }
+
+            // Clean up Veldrid resources
+            gd.WaitForIdle();
+            controller.Dispose();
+            cl.Dispose();
+            gd.Dispose();
+        }
+
+        static void RenderUI()
+        {
+            ImGuiViewportPtr viewportptr = ImGui.GetMainViewport();
+            ImGui.SetNextWindowPos(viewportptr.WorkPos);
+            ImGui.SetNextWindowSize(viewportptr.WorkSize);
+            ImGui.SetNextWindowViewport(viewportptr.ID);
+
+            if (ImGui.Begin("Main", ImGuiWindowFlags.NoTitleBar | ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoMove))
+            {
+                ImGui.TextUnformatted("hello");
+            }
+            ImGui.End();
+        }
+    }
+}
